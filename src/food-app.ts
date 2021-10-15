0d093f27b024d13ae87057c3a12bb7e55e21c171
IHprogram <ihm20202020@gmail.com>
2021-10-15 17:24:48 +0900

    クリックしたdiv要素にfood--activeと言うクラス名を加える処理の実装

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,3 +1,22 @@
 class Score { }
-class Food { }
-class Foods { }
+
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener('click', this.clickEventHandler.bind(this)); // clickEventHandler内のthisは、constructor内で使用されているthisと同じだ、と明示的にしている
+  }
+  clickEventHandler() {
+    console.log(this)
+    this.element.classList.toggle('food--active');
+  }
+}
+
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  constructor() {
+    this.elements.forEach(element => {
+      new Food(element);
+    })
+  }
+}
+
+const foods = new Foods();
