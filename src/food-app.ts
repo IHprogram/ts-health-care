5b870f3b42214bd437861c64db255c9aca8a6d4d
IHprogram <ihm20202020@gmail.com>
2021-10-19 16:27:18 +0900

    interfaceを使用

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,4 +1,20 @@
-class Score {
+interface Scoreable {
+  readonly totalScore: number;
+  render(): void;
+}
+
+interface Foodable {
+  element: HTMLDivElement;
+  clickEventHandler(): void;
+}
+
+interface Foodsable {
+  elements: NodeListOf<HTMLDivElement>;
+  readonly activeElements: HTMLDivElement[];
+  readonly activeElementsScore: number[];
+}
+
+class Score implements Scoreable {
   private static instance: Score;
   get totalScore() {
     const foods = Foods.getInstance();
@@ -20,7 +36,7 @@ class Score {
   }
 }
 
-class Food {
+class Food implements Foodable {
   constructor(public element: HTMLDivElement) {
     element.addEventListener('click', this.clickEventHandler.bind(this)); // clickEventHandler内のthisは、constructor内で使用されているthisと同じだ、と明示的にしている
   }
@@ -31,7 +47,7 @@ class Food {
   }
 }
 
-class Foods {
+class Foods implements Foodsable {
   private static instance: Foods;
   elements = document.querySelectorAll<HTMLDivElement>('.food');
   private _activeElements: HTMLDivElement[] = []; // active(food--active)になった要素が入る
